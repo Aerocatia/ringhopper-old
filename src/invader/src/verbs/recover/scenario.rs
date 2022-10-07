2a7d77a16a6205b52e268e19f236b9134e63c6d7
Snowy <snowy@snowymouse.com>
2022-10-07 19:17:57 +0200

    Check for duplicate scripts before recovery
    Can happen with global_scripts on stock tools.

--- src/invader/src/verbs/recover/scenario.rs
+++ src/invader/src/verbs/recover/scenario.rs
@@ -2,7 +2,9 @@ use ringhopper::error::*;
 use ringhopper::file::TagFile;
 use ringhopper::engines::h1::definitions::*;
 use ringhopper::engines::h1::TagFileSerializeFn;
+use ringhopper::types::ReflexiveFn;
 use std::path::Path;
+use strings::*;
 use crate::file::*;
 use super::RecoverResult;
 
@@ -13,9 +15,19 @@ pub fn recover_scripts(tag_data: &[u8], tag_file: &TagFile, data_dir: &Path, ove
         return Ok(RecoverResult::NoSourceData);
     }
 
+    // Check if we have duplicate source files of the same name.
+    for i in 1..tag.source_files.len() {
+        let this_name = tag.source_files[i].name.to_str();
+        for j in &tag.source_files.blocks[0..i] {
+            if this_name == j.name.to_str() {
+                return Err(ErrorMessage::AllocatedString(format!(get_compiled_string!("engine.h1.verbs.recover.error_duplicate_scripts"), name=this_name)));
+            }
+        }
+    }
+
     // If the scenario consists of only global_scripts, we should not make a directory.
     //
-    // For example, timberland which only has global_scripts but no actual scripts.
+    // For example, timberland only has global_scripts but no actual scripts.
     let output_dir = data_dir.join(&tag_file.tag_path.to_string()).parent().unwrap().join("scripts");
     for s in &tag.source_files {
         if s.name.to_str() != "global_scripts" {
