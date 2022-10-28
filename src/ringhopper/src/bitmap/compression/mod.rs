42c775416995b99abcb976529f35a791ef5bec1e
Snowy <snowy@snowymouse.com>
2022-10-28 21:32:36 +0200

    Configure texpresso to use equal channels

--- src/ringhopper/src/bitmap/compression/mod.rs
+++ src/ringhopper/src/bitmap/compression/mod.rs
@@ -269,6 +269,7 @@ impl BitmapEncoding {
                     // Set up our parameters
                     let mut params = Params::default();
                     params.algorithm = Algorithm::IterativeClusterFit;
+                    params.weights = [1.0, 1.0, 1.0]; // weigh each channel equally (may look slightly worse on some textures but better for multipurposes)
                     let mut rgba = BitmapEncoding::A8B8G8R8.encode(pixels, width, height, depth, faces, mipmaps, false);
                     const UNCOMPRESSED_BPP: usize = BitmapEncoding::A8B8G8R8.bits_per_pixel() / 8;
 
